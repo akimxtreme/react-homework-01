478e66fee0ed92124b24579fafb80af34e31399b
Domingo Ilarreta <domingo.ilarreta@gmail.com>
2021-02-07 19:33:59 -0300

    Cambios

---
+++ src/components/element/element.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './element.styles.scss';
+
+class Element extends React.Component {
+    constructor(){
+        
+    }
+};
+
+export default Element;
---
+++ src/components/elements/elements.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Element from '../element/element.component';
+import LIVING_BEINGS_DATA from '../data/living-beings.data';
+import './elements.styles.scss';
+
+class Elements extends React.Component {
+    constructor(){
+        super();
+    }
+};
+
+export default Elements;
