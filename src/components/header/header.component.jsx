e4d21dc60d83af7c581f0cb1837818154bc89228
Domingo Ilarreta <domingo.ilarreta@gmail.com>
2021-02-07 19:14:48 -0300

    Cambios

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import './header.styles.scss';
 
-const Header = (currentUser) => (
-    <p>Header</p>
+const Header = ({ currentUser }) => (
+    <div className='header'>
+        <Link className='logo-container' to="/">
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/'>Inicio</Link>
+            <Link className='option' to='/living-beings'>Reino Animal</Link>
+            { currentUser ? (
+                <Link className='option' to='/'>Cerrar Sesión</Link>
+            ) : (
+                <Link className='option' to='/'>Login</Link>
+            ) }
+        </div>
+    </div>
 );
 
 export default Header;
