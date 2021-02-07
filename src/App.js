3330d176b6ea23b6f91c0b913fc425abc0da66a4
Domingo Ilarreta <domingo.ilarreta@gmail.com>
2021-02-07 18:14:51 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Page
+import HomePage from './pages/home/home-page.component';
+import LivingBeingPage from './pages/living-beings/living-beings.component'
+
+// Component
+import Header from './components/header/header.component';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount(){}
+
+  componentWillUnmount(){}
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={ this.state.currentUser } />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/living-beings" component={LivingBeingPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/living-beings/living-beings.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LivingBeingPage = () => (
+    <p>LivingBeingPage</p>
+);
+
+export default LivingBeingPage;
