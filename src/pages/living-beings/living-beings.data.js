2123e723151205b60a3f4fb489e09d815b9d2e0a
Domingo Ilarreta <domingo.ilarreta@gmail.com>
2021-02-07 17:03:00 -0300

    Cambios

---
+++ src/pages/living-beings/living-beings.data.js
@@ -0,0 +1,40 @@
+/* Seres Vivos */
+const LIVING_BEINGS_DATA = [
+    {
+        "_id": "5fe96e52e5b4fa3ee7a91e59",
+        "name": "Animales",
+        "description": "Los animales son seres pluricelulares y eucariotas de alimentación heterótrofa, respiración aeróbica, reproducción sexual y capacidad de desplazamiento.",
+        "createdAt": "2020-12-28T05:34:10.643Z",
+        "updatedAt": "2020-12-28T06:32:42.619Z"
+    },
+    {
+        "_id": "5fedfcf975ffd517ccd26268",
+        "name": "Vegetal",
+        "description": "Los árboles, las plantas y demás especies vegetales forman parte del reino Plantae, uno de los más antiguos y que se caracteriza por su naturaleza inmóvil, pluricelular y eucariota.",
+        "createdAt": "2020-12-31T16:31:53.298Z",
+        "updatedAt": "2020-12-31T16:31:53.298Z"
+    },
+    {
+        "_id": "5fedfd3375ffd517ccd26269",
+        "name": "Funji",
+        "description": "Este nombre se utiliza para designar al reino de los hongos, que contempla a las levaduras, los mohos y todas las especies de setas.",
+        "createdAt": "2020-12-31T16:32:51.101Z",
+        "updatedAt": "2020-12-31T16:32:51.101Z"
+    },
+    {
+        "_id": "5fedfd7e75ffd517ccd2626a",
+        "name": "Protoctista",
+        "description": "Este grupo es el más primitivo de los eucariontes y de él provendrían todos los demás. El reino Protista es parafilético, contiene al ancestro común pero no a todos sus descendientes y engloba a aquellos organismos eucariotas que no se consideran ni animales, ni plantas ni hongos, como los protozoos.",
+        "createdAt": "2020-12-31T16:34:06.429Z",
+        "updatedAt": "2020-12-31T16:34:06.429Z"
+    },
+    {
+        "_id": "5fedfda175ffd517ccd2626b",
+        "name": "Monera",
+        "description": "Es el reino de los seres vivos microscópicos y aglutina a los organismos procariotas (arqueas y bacterias). Este grupo está presente en todos los hábitats y lo forman seres unicelulares sin núcleo definido.",
+        "createdAt": "2020-12-31T16:34:41.553Z",
+        "updatedAt": "2020-12-31T16:34:41.553Z"
+    }
+];
+  
+export default LIVING_BEINGS_DATA;
